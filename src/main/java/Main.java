f0ba7cf374d68aca39edc32df34be17dc58a7c6c
Nelson <nelson.alexander@gmail.com>
2016-12-10 15:38:39 -0500

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+/**
+ * Created by nelsonramirez on 12/10/16.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello domain");
+    }
+
+    public static void foo() {
+
+    }
+
+    public static void bar() {
+        
+    }
+
+}
